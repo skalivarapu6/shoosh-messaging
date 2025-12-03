3b87c0634d8e8dd0bd47f94463b5028e4c267977
Sai Shaket Kalivarapu <skali@Sais-MacBook-Pro-2.local>
2025-12-03 12:36:34 -0700

    cleanup: remove unnecessary files and build artifacts

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const CredentialManager = artifacts.require("CredentialManager");
-const DIDRegistry = artifacts.require("DIDRegistry");
-const MessageMetadata = artifacts.require("MessageMetadata");
-
-module.exports = async function (deployer) {
-  await deployer.deploy(DIDRegistry);
-  await deployer.deploy(CredentialManager);
-  await deployer.deploy(MessageMetadata);
-};
